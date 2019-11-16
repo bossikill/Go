e4b1b55bd08c6fad518320de02c8cdab522bbf10
bossikill <550476877@qq.com>
2019-11-17 01:42:45 +0800

    omiited

--- 0.test/test.go
+++ 0.test/test.go
@@ -1,19 +1,9 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"path"
-)
-
 func init() {
 
 }
 
 func main() {
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		fmt.Println(path.Ext(input.Text()))
-	}
+
 }
